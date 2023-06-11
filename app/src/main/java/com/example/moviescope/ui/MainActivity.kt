730b3d81377cdd8697e35619719df9322f0d032d
ulusalemin56 <mulusal456@gmail.com>
2023-06-11 18:26:04 +0300

    Fragments and package files are added.

--- app/src/main/java/com/example/moviescope/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviescope/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moviescope
+package com.example.moviescope.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviescope.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
