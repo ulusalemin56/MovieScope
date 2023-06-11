ee78736af7847b07380c31ed160d626629dcf068
ulusalemin56 <mulusal456@gmail.com>
2023-06-12 01:13:34 +0300

    Bottom Navigation component and ViewBinding are added.

--- app/src/main/java/com/example/moviescope/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviescope/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.moviescope.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.moviescope.R
+import com.example.moviescope.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Bottom Navigation component is added.
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView= binding.bottomNavigationView           //findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView, navController)
+
     }
 }
--- app/src/main/java/com/example/moviescope/ui/favorites_fragment/FavoritesFragment.kt
+++ app/src/main/java/com/example/moviescope/ui/favorites_fragment/FavoritesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.moviescope.ui.favourites_fragment
+package com.example.moviescope.ui.favorites_fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,14 +8,14 @@ import android.view.ViewGroup
 import com.example.moviescope.R
 
 
-class FavouritesFragment : Fragment() {
+class FavoritesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourites, container, false)
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 
 }
