40dbdc9bd7ed460196531bd50540e3e54022ec53
ulusalemin56 <mulusal456@gmail.com>
2023-06-14 02:37:35 +0300

    The missing features in the design were added.

--- app/src/main/java/com/example/moviescope/ui/home_fragment/HomeFragment.kt
+++ app/src/main/java/com/example/moviescope/ui/home_fragment/HomeFragment.kt
@@ -6,15 +6,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.moviescope.R
+import com.example.moviescope.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding : FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+
+
+        initRecylerView()
+
+
+        return binding.root
+    }
+
+    private fun initRecylerView() {
+        val imageMovieModels = getListImageMovie()
+        val adapter = ImageMovieAdapter(imageMovieModels)
+        binding.apply {
+            mostPopMoviesRecyclerView.adapter = adapter
+            topRatedMoviesRecyclerView.adapter = adapter
+            mostPopSeriesRecyclerView.adapter = adapter
+            topBoxofficeRecyclerView.adapter = adapter
+        }
+    }
+
+    private fun getListImageMovie(): List<ImageMovieModel> {
+        return mutableListOf(
+            ImageMovieModel(R.drawable.deneme_poster),
+            ImageMovieModel(R.drawable.deneme_poster2),
+            ImageMovieModel(R.drawable.deneme_poster3),
+            ImageMovieModel(R.drawable.deneme_poster),
+            ImageMovieModel(R.drawable.deneme_poster2),
+            ImageMovieModel(R.drawable.deneme_poster3),
+            ImageMovieModel(R.drawable.deneme_poster),
+            ImageMovieModel(R.drawable.deneme_poster2),
+            ImageMovieModel(R.drawable.deneme_poster3),
+        )
     }
 
 }
