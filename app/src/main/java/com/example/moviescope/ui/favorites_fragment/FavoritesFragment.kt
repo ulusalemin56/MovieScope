ab5b00090689c744772202a8d4b25decd1faef26
ulusalemin56 <mulusal456@gmail.com>
2023-07-11 00:29:56 +0300

    Add the ViewModel of Favorites Fragment.

--- app/src/main/java/com/example/moviescope/ui/favorites_fragment/FavoritesFragment.kt
+++ app/src/main/java/com/example/moviescope/ui/favorites_fragment/FavoritesFragment.kt
@@ -5,17 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.moviescope.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.moviescope.data.model.local.BookmarkEntity
+import com.example.moviescope.databinding.FragmentFavoritesBinding
+import com.example.moviescope.util.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class FavoritesFragment : Fragment() {
-
+    private lateinit var binding: FragmentFavoritesBinding
+    private val viewModel: FavoritesViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    ): View {
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        initCollect()
+        return binding.root
     }
 
+    private fun initCollect() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.fetchMediaFromBookmarks.collectLatest {
+                    when (it) {
+                        is Resource.Loading -> {}
+                        is Resource.Success -> {
+                           initFavoritesRV(it.data)
+                        }
+
+                        is Resource.Error -> {}
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initFavoritesRV(bookmarks: List<BookmarkEntity>) {
+
+    }
 }
---
+++ app/src/main/java/com/example/moviescope/ui/favorites_fragment/FavoritesViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.moviescope.ui.favorites_fragment
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.moviescope.data.model.local.BookmarkEntity
+import com.example.moviescope.data.repo.MovieScopeRepository
+import com.example.moviescope.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(
+    repository : MovieScopeRepository
+) : ViewModel() {
+
+    val fetchMediaFromBookmarks : StateFlow<Resource<List<BookmarkEntity>>> =
+        repository.fetchMediaFromBookmarks().stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = Resource.Loading
+        )
+}
